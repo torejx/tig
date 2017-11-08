c7cadbfd063601d2d88cfa0deed9e924cab66273
tore@interlogica <walter.dorian@gmail.com>
2017-11-08 15:26:18 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='tig',
+      version='0.1',
+      description='TiG: TiG isn\'t Git',
+      author='Salvatore Caputi',
+      author_email='salvatore@scaputi.net',
+      #url='https://www.python.org/sigs/distutils-sig/',
+      packages=['tests', 'tig']
+     )
