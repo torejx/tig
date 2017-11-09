a4da9b764f8ba7d74aee556cc52ff4643f17e8c7
tore@interlogica <walter.dorian@gmail.com>
2017-11-09 16:24:17 +0100

    edit tests

--- tests/test.py
+++ tests/test.py
@@ -39,19 +39,9 @@ class TestTig(unittest.TestCase):
         self.assertTrue(os.path.exists('.tig/tmp'))
 
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_get_current_branch(self):
+        self.test_init_ok()
+        self.assertEqual(get_current_branch(), 'master')
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestTig)
